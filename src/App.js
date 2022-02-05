363d6a25acbe9921c6c15ad5c22e7434aff3d177
GuiManfrin134 <manfrin11gui@gmail.com>
2022-02-05 19:02:59 -0300

    Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import Routex from "./routex";
 import Header from "./components/Header";
 
+import store from "./store";
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routex />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routex />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
